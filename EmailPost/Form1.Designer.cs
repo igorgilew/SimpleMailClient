980d42f9c3164b60776a43cae45bf5e8e2f75d59
igorgilew <ig.gilew2013@gmail.com>
2019-01-15 09:39:11 +0500

    Реализована отправка писем

---
+++ EmailPost/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace EmailPost
+{
+    partial class EmailPost
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbAdress = new System.Windows.Forms.TextBox();
+            this.rtbContent = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbAdress
+            // 
+            this.tbAdress.Location = new System.Drawing.Point(45, 66);
+            this.tbAdress.Name = "tbAdress";
+            this.tbAdress.Size = new System.Drawing.Size(406, 20);
+            this.tbAdress.TabIndex = 0;
+            // 
+            // rtbContent
+            // 
+            this.rtbContent.Location = new System.Drawing.Point(45, 124);
+            this.rtbContent.Name = "rtbContent";
+            this.rtbContent.Size = new System.Drawing.Size(406, 154);
+            this.rtbContent.TabIndex = 1;
+            this.rtbContent.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(45, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Адрес получателя:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(45, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Текст сообщения:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSend.Location = new System.Drawing.Point(174, 293);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(172, 51);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(979, 416);
+            this.splitContainer1.SplitterDistance = 464;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // EmailPost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 440);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbAdress);
+            this.Controls.Add(this.rtbContent);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "EmailPost";
+            this.Text = "EmailPost";
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbAdress;
+        private System.Windows.Forms.RichTextBox rtbContent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ EmailPost/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+namespace EmailPost
+{
+    public partial class EmailPost : Form
+    {
+        public EmailPost()
+        {
+            InitializeComponent();
+        }
+        string fromAdress = "yatiia@yandex.ru";
+        private void SendMessageBySMTP()
+        {
+            var message = CreateMailMessage();
+            // адрес smtp-сервера и порт, с которого будем отправлять письмо
+            SmtpClient smtp = new SmtpClient("smtp.yandex.ru", 587);
+            // логин и пароль
+            smtp.Credentials = new NetworkCredential("yatiia@yandex.ru", "qwerty11");
+            smtp.EnableSsl = true;
+            smtp.Send(message);
+        }
+        
+        private MailMessage CreateMailMessage()
+        {          
+            // создаем объект сообщения
+            return new MailMessage(fromAdress, tbAdress.Text, "TestMessaging", rtbContent.Text);                        
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            SendMessageBySMTP();
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
